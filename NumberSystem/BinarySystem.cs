fb7bd42f868fffd01cdee4a97021f6d8fa4ba1ee
stas <Stas.pineapplee@yandex.ru>
2021-05-26 15:16:01 +0300

    Binary and Octal System added

---
+++ NumberSystem/BinarySystem.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TrySystem
+{
+    class BinarySystem:ISystem
+    {
+        int dec;
+
+        List<int> result = new List<int> { };
+        public BinarySystem(int number)
+        {
+            dec = number;
+        }
+
+        public BinarySystem(List<int> result)
+        {
+            this.result = result;
+        }
+
+        public void FromDecimalToBinary()
+        {
+            while (dec>=2)
+            {
+                result.Add(dec % 2);
+                dec = (dec - dec % 2) / 2;
+            }
+            if (dec != 0)
+            {
+                result.Add(dec);
+            }
+            
+        }
+
+        public void FromBinaryToDecimal()
+        {
+            
+            dec = 0;
+            
+            for (int i = 0; i < result.Count; i++)
+            {
+                dec+= result[i]*(int)Math.Pow(2, i);
+            }
+        }
+
+        public void Print()
+        {
+            for (int i = result.Count - 1; i >= 0; i--)
+            {
+                Console.Write(result[i]);
+            }
+        }
+        public void PrintDecimal()
+        {
+
+            Console.WriteLine(dec);
+
+        }
+
+
+    }
+}
---
+++ NumberSystem/OctalSystem.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrySystem
+{
+    class OctalSystem: ISystem
+    {
+        int dec;
+
+        List<int> result = new List<int> { };
+
+        public OctalSystem(int number)
+        {
+            dec = number;
+        }
+
+        public OctalSystem(List<int> number)
+        {
+            result = number;
+        }
+
+        public void FromDecimalToOctal()
+        {
+            while (dec >= 8)
+            {
+                result.Add(dec % 8);
+                dec = (dec - dec % 8) / 8;
+            }
+            if (dec != 0)
+            {
+                result.Add(dec);
+            }
+        }
+
+        public void FromOctalToDecimal()
+        {
+            dec = 0;
+
+            for (int i = 0; i < result.Count; i++)
+            {
+                dec += result[i] * (int)Math.Pow(8, i);
+            }
+        }
+
+        public void Print()
+        {
+            for (int i = result.Count - 1; i >= 0; i--)
+            {
+                Console.Write(result[i]);
+            }
+        }
+        public void PrintDecimal()
+        {
+
+            Console.WriteLine(dec);
+
+        }
+    }
+}
